2cf38d576761bbe916c7c5246b970eafcbfd790e
ZWT006 <zwt190315@163.com>
2026-01-28 19:33:18 +0900

    <test>

--- robots/beetle_omni/scripts/rl/pub_waypoints.py
+++ robots/beetle_omni/scripts/rl/pub_waypoints.py
@@ -31,11 +31,11 @@ from geometry_msgs.msg import PoseStamped, Quaternion
 # ]
 
 # 45 Roll Test Waypoints
-# WAYPOINTS = [
-#     [0.0, 0.0, 0.7, 0.0, 0.0, 0.0],    # Hover at 1m
-#     [0.8, 0.0, 0.7, -45.0, 0.0, 0.0],    # Hover at 1m
-#     [0.0, 0.0, 0.55, 0.0, 0.0, 0.0],    # Return origin, Down
-# ]
+WAYPOINTS = [
+    [0.0, 0.0, 0.8, 0.0, 0.0, 0.0],    # Hover at 1m
+    [0.8, 0.0, 0.8, -45.0, 0.0, 0.0],    # Hover at 1m
+    [0.0, 0.0, 0.55, 0.0, 0.0, 0.0],    # Return origin, Down
+]
 
 ######################################################################
 # want to show large tilt (back)
@@ -51,14 +51,14 @@ from geometry_msgs.msg import PoseStamped, Quaternion
 # ]
 
 # want to show large tilt
-WAYPOINTS = [
-    [0.0, 0.0, 0.8, 0.0, 0.0, 0.0],    # Hover at 1m
-    [1.0, 0.0, 1.0, 45.0, 0.0, 0.0],    # Move X+
-    [0.0, 1.0, 1.0, 0.0, -25.0, 0.0],    # Move X+
-    [-1.0, 0.0, 1.0, -25.0, 0.0, 0.0],   # Move Y+, Up, Yaw 90
-    [0.0, 0.0, 0.8, 0.0, 0.0, 90.0],
-    [0.0, 0.0, 0.8, 0.0, 0.0, 0.0],  # Move X-, Yaw 180
-]
+# WAYPOINTS = [
+#     [0.0, 0.0, 0.8, 0.0, 0.0, 0.0],    # Hover at 1m
+#     [1.0, 0.0, 1.0, 45.0, 0.0, 0.0],    # Move X+
+#     [0.0, 1.0, 1.0, 0.0, -25.0, 0.0],    # Move X+
+#     [-1.0, 0.0, 1.0, -25.0, 0.0, 0.0],   # Move Y+, Up, Yaw 90
+#     [0.0, 0.0, 0.8, 0.0, 0.0, 90.0],
+#     # [0.0, 0.0, 0.8, 0.0, 0.0, 0.0],  # Move X-, Yaw 180
+# ]
 
 # Challenge Waypoints
 # WAYPOINTS = [
